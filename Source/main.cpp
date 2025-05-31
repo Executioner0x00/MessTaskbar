baa566f90185ffde1570e0844b8345838c5814b7
Executioner <diamondintheruffvendorevents@outlook.com>
2025-05-31 02:35:22 -0400

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,7 +4,7 @@ INT main() {
 	HWND hWindow = FindWindow("Shell_TrayWnd"), NULL); // Search for the window
         
         if (hWindow) {
-	    ShowWindow(hWindow, SW_HIDE); // Hide taskbar, can be replaced with SW_MAX, SW_MINIMIZE etc.
+	      ShowWindow(hWindow, SW_HIDE); // Hide taskbar, can be replaced with SW_MAX, SW_MINIMIZE etc.
         }
 	return 0;
 }
